78fd1c45430e145d166038913c3467f8919900a7
JianBo He <heeejianbo@gmail.com>
2020-09-28 13:46:56 +0800

    tests(proto): add more and more test case

--- test/emqx_exproto_echo_svr.erl
+++ test/emqx_exproto_echo_svr.erl
@@ -22,9 +22,21 @@
         , stop/1
         ]).
 
+-export([ frame_connect/2
+        , frame_connack/1
+        , frame_publish/3
+        , frame_puback/1
+        , frame_subscribe/2
+        , frame_suback/1
+        , frame_unsubscribe/1
+        , frame_unsuback/1
+        , frame_disconnect/0
+        ]).
+
 -export([ on_socket_created/2
         , on_received_bytes/2
         , on_socket_closed/2
+        , on_timer_timeout/2
         , on_received_messages/2
         ]).
 
@@ -39,9 +51,20 @@
 -define(send(Req), ?CLIENT:send(Req, #{channel => ct_test_channel})).
 -define(close(Req), ?CLIENT:close(Req, #{channel => ct_test_channel})).
 -define(authenticate(Req), ?CLIENT:authenticate(Req, #{channel => ct_test_channel})).
+-define(start_timer(Req), ?CLIENT:start_timer(Req, #{channel => ct_test_channel})).
 -define(publish(Req), ?CLIENT:publish(Req, #{channel => ct_test_channel})).
 -define(subscribe(Req), ?CLIENT:subscribe(Req, #{channel => ct_test_channel})).
 -define(unsubscribe(Req), ?CLIENT:unsubscribe(Req, #{channel => ct_test_channel})).
+
+-define(TYPE_CONNECT, 1).
+-define(TYPE_CONNACK, 2).
+-define(TYPE_PUBLISH, 3).
+-define(TYPE_PUBACK, 4).
+-define(TYPE_SUBSCRIBE, 5).
+-define(TYPE_SUBACK, 6).
+-define(TYPE_UNSUBSCRIBE, 7).
+-define(TYPE_UNSUBACK, 8).
+-define(TYPE_DISCONNECT, 9).
 
 %%--------------------------------------------------------------------
 %% APIs
@@ -75,18 +98,9 @@ on_socket_created(Ctx, Req) ->
     {ok, emqx_exproto_pb:bool_result(), ctx:ctx()} | grpcbox_stream:grpc_error_response().
 on_received_bytes(Ctx, Req = #{conn := Conn, bytes := Bytes}) ->
     io:format("~p: ~0p~n", [?FUNCTION_NAME, Req]),
-    ClientInfo = #{proto_name => <<"echo">>,
-                   proto_ver => <<"0.1">>,
-                   clientid => <<"echo_conn_1">>
-                  },
-    case ?authenticate(#{conn => Conn, clientinfo => ClientInfo}) of
-        {ok, #{result := true}, _} ->
-            ?subscribe(#{conn => Conn, topic => <<"t/#">>, qos => 0}),
-            ?send(#{conn => Conn, bytes => Bytes}),
-            {ok, #{}, Ctx};
-        _ ->
-            {ok, #{}, Ctx}
-    end.
+    #{<<"type">> := Type} = Params = emqx_json:decode(Bytes, [return_maps]),
+    _ = handle_in(Conn, Type, Params),
+    {ok, #{}, Ctx}.
 
 -spec on_socket_closed(ctx:ctx(), emqx_exproto_pb:socket_closed_request()) ->
     {ok, emqx_exproto_pb:empty_success(), ctx:ctx()} | grpcbox_stream:grpc_error_response().
@@ -94,11 +108,131 @@ on_socket_closed(Ctx, Req) ->
     io:format("~p: ~0p~n", [?FUNCTION_NAME, Req]),
     {ok, #{}, Ctx}.
 
+on_timer_timeout(Ctx, Req = #{conn := Conn, type := 'KEEPALIVE'}) ->
+    io:format("~p: ~0p~n", [?FUNCTION_NAME, Req]),
+    handle_out(Conn, ?TYPE_DISCONNECT),
+    ?close(Conn),
+    {ok, #{}, Ctx}.
+
 -spec on_received_messages(ctx:ctx(), emqx_exproto_pb:received_messages_request()) ->
     {ok, emqx_exproto_pb:bool_result(), ctx:ctx()} | grpcbox_stream:grpc_error_response().
 on_received_messages(Ctx, Req = #{conn := Conn, messages := Messages}) ->
     io:format("~p: ~0p~n", [?FUNCTION_NAME, Req]),
-    lists:foreach(fun(#{topic := Topic, qos := Qos, payload := Payload}) ->
-        ?publish(#{conn => Conn, topic => Topic, qos => Qos, payload => Payload})
+    lists:foreach(fun(Message) ->
+        handle_out(Conn, ?TYPE_PUBLISH, Message)
     end, Messages),
     {ok, #{}, Ctx}.
+
+%%--------------------------------------------------------------------
+%% The Protocol Example:
+%%  CONN:
+%%   {"type": 1, "clientinfo": {...}}
+%%
+%%  CONNACK:
+%%   {"type": 2, "code": 0}
+%%
+%%  PUBLISH:
+%%   {"type": 3, "topic": "xxx", "payload": "", "qos": 0}
+%%
+%%  PUBACK:
+%%   {"type": 4, "code": 0}
+%%
+%%  SUBSCRIBE:
+%%   {"type": 5, "topic": "xxx", "qos": 1}
+%%
+%%  SUBACK:
+%%   {"type": 6, "code": 0}
+%%
+%%  DISCONNECT:
+%%   {"type": 7, "code": 1}
+%%--------------------------------------------------------------------
+
+handle_in(Conn, ?TYPE_CONNECT, #{<<"clientinfo">> := ClientInfo, <<"password">> := Password}) ->
+    NClientInfo = maps:from_list([{binary_to_atom(K, utf8), V} || {K, V} <- maps:to_list(ClientInfo)]),
+    case ?authenticate(#{conn => Conn, clientinfo => NClientInfo, password => Password}) of
+        {ok, #{result := true}, _} ->
+            case maps:get(keepalive, NClientInfo, 0) of
+                0 -> ok;
+                Intv ->
+                    io:format("Try call start_timer with ~ps", [Intv]),
+                    ?start_timer(#{conn => Conn, type => 'KEEPALIVE', interval => Intv})
+            end,
+            handle_out(Conn, ?TYPE_CONNACK, 0);
+        _ ->
+            handle_out(Conn, ?TYPE_CONNACK, 1),
+            ?close(Conn)
+    end;
+handle_in(Conn, ?TYPE_PUBLISH, #{<<"topic">> := Topic,
+                                 <<"qos">> := Qos,
+                                 <<"payload">> := Payload}) ->
+    case ?publish(#{conn => Conn, topic => Topic, qos => Qos, payload => Payload}) of
+        {ok, #{result := true}, _} ->
+            handle_out(Conn, ?TYPE_PUBACK, 0);
+        _ ->
+            handle_out(Conn, ?TYPE_PUBACK, 1)
+    end;
+handle_in(Conn, ?TYPE_SUBSCRIBE, #{<<"qos">> := Qos, <<"topic">> := Topic}) ->
+    case ?subscribe(#{conn => Conn, topic => Topic, qos => Qos}) of
+        {ok, #{result := true}, _} ->
+            handle_out(Conn, ?TYPE_SUBACK, 0);
+        _ ->
+            handle_out(Conn, ?TYPE_SUBACK, 1)
+    end;
+handle_in(Conn, ?TYPE_UNSUBSCRIBE, #{<<"topic">> := Topic}) ->
+    case ?unsubscribe(#{conn => Conn, topic => Topic}) of
+        {ok, #{result := true}, _} ->
+            handle_out(Conn, ?TYPE_UNSUBACK, 0);
+        _ ->
+            handle_out(Conn, ?TYPE_UNSUBACK, 1)
+    end;
+
+handle_in(Conn, ?TYPE_DISCONNECT, _) ->
+    ?close(#{conn => Conn}).
+
+handle_out(Conn, ?TYPE_CONNACK, Code) ->
+    ?send(#{conn => Conn, bytes => frame_connack(Code)});
+handle_out(Conn, ?TYPE_PUBACK, Code) ->
+    ?send(#{conn => Conn, bytes => frame_puback(Code)});
+handle_out(Conn, ?TYPE_SUBACK, Code) ->
+    ?send(#{conn => Conn, bytes => frame_suback(Code)});
+handle_out(Conn, ?TYPE_UNSUBACK, Code) ->
+    ?send(#{conn => Conn, bytes => frame_unsuback(Code)});
+handle_out(Conn, ?TYPE_PUBLISH, #{qos := Qos, topic := Topic, payload := Payload}) ->
+    ?send(#{conn => Conn, bytes => frame_publish(Topic, Qos, Payload)}).
+
+handle_out(Conn, ?TYPE_DISCONNECT) ->
+    ?send(#{conn => Conn, bytes => frame_disconnect()}).
+
+%%--------------------------------------------------------------------
+%% Frame
+
+frame_connect(ClientInfo, Password) ->
+    emqx_json:encode(#{type => ?TYPE_CONNECT,
+                       clientinfo => ClientInfo,
+                       password => Password}).
+frame_connack(Code) ->
+    emqx_json:encode(#{type => ?TYPE_CONNACK, code => Code}).
+
+frame_publish(Topic, Qos, Payload) ->
+    emqx_json:encode(#{type => ?TYPE_PUBLISH,
+                       topic => Topic,
+                       qos => Qos,
+                       payload => Payload}).
+
+frame_puback(Code) ->
+    emqx_json:encode(#{type => ?TYPE_PUBACK, code => Code}).
+
+frame_subscribe(Topic, Qos) ->
+    emqx_json:encode(#{type => ?TYPE_SUBSCRIBE, topic => Topic, qos => Qos}).
+
+frame_suback(Code) ->
+    emqx_json:encode(#{type => ?TYPE_SUBACK, code => Code}).
+
+frame_unsubscribe(Topic) ->
+    emqx_json:encode(#{type => ?TYPE_UNSUBSCRIBE, topic => Topic}).
+
+frame_unsuback(Code) ->
+    emqx_json:encode(#{type => ?TYPE_UNSUBACK, code => Code}).
+
+frame_disconnect() ->
+    emqx_json:encode(#{type => ?TYPE_DISCONNECT}).
